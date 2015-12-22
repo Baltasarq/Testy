8da5f2e94bf6657156b57b08bba2a567e047a3b0
baltasarq <baltasarq@gmail.com>
2015-12-22 23:07:26 +0100

    Got rid of the designer for the main window.

--- Core/AppInfo.cs
+++ Core/AppInfo.cs
@@ -5,7 +5,7 @@ namespace Testy.Core {
 	public class AppInfo
 	{
 		public const string Name = "Testy";
-		public const string Version = "v1.1.2 20151108";
+		public const string Version = "v1.2.0 20151222";
 		public const string Author = "baltasarq";
 		public const string Email = "baltasarq@gmail.com";
 		public const string Website = "http://baltasarq.info/dev/testy/";
