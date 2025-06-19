04968fa6e86ba37dbb569a4769045d2d1d9d9a2e
baltasar <baltasarq@gmail.com>
2025-06-19 10:00:17 +0200

    Bumps version

--- Core/AppInfo.cs
+++ Core/AppInfo.cs
@@ -1,10 +1,11 @@
-using System;
+// Testy (c) 2017-2025 Baltasar MIT License <baltasarq@gmail.com>
+
 
 namespace Testy.Core {
 
 	public static class AppInfo {
 		public const string Name = "Testy";
-		public const string Version = "v1.4.2 20170922";
+		public const string Version = "v1.4.3 20250611";
 		public const string Author = "baltasarq";
 		public const string Email = "baltasarq@gmail.com";
 		public const string Website = "http://baltasarq.info/dev/testy/";
