4fb588be23fbd5984808c3955bfaa6ebb755ece8
baltasarq <baltasarq@gmail.com>
2026-05-28 13:26:39 +0200

    Bumped version

--- Core/AppInfo.cs
+++ Core/AppInfo.cs
@@ -4,8 +4,9 @@
 namespace Testy.Core {
 
 	public static class AppInfo {
+		public const string Id = "com.devbaltasarq.testy";
 		public const string Name = "Testy";
-		public const string Version = "v1.4.3 20250611";
+		public const string Version = "v1.4.4 20260509";
 		public const string Author = "baltasarq";
 		public const string Email = "baltasarq@gmail.com";
 		public const string Website = "http://baltasarq.info/dev/testy/";
